ecd4f1aa64e0d4e907d8ca126562d166cf4360ea
VZout <viktor.zoutman@embark-studios.com>
2023-09-06 18:05:48 +0200

    fix linux build

--- fsr2-sys/build/bindgen.rs
+++ fsr2-sys/build/bindgen.rs
@@ -59,7 +59,7 @@ pub fn generate_vk_bindings(api_dir: &str, vk_include_dir: &str) {
     let wrapper_api = format!("{}/vk/ffx_fsr2_vk.h", api_dir);
 
     // Generate bindings
-    let bindings = bindgen::Builder::default()
+    let mut bindings = bindgen::Builder::default()
         .layout_tests(false)
         .derive_default(true)
         .prepend_enum_name(false)
@@ -70,9 +70,13 @@ pub fn generate_vk_bindings(api_dir: &str, vk_include_dir: &str) {
         .parse_callbacks(Box::new(Renamer))
         .clang_arg(format!("-I{}", vk_include_dir))
         .allowlist_recursively(false)
-        .allowlist_file(&wrapper_api)
-        .generate()
-        .expect("Unable to generate bindings");
+        .allowlist_file(&wrapper_api);
+
+    if cfg!(not(target_os = "windows")) {
+        bindings = bindings.clang_args(["-DFFX_GCC", "-fshort-wchar"]);
+    }
+
+    let bindings = bindings.generate().expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
